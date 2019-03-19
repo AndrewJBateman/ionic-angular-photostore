c09ec0517ad2e6dc36356c3784394f82ccd94522
Andrew Bateman <gomezbateman@yahoo.com>
2019-03-19 16:16:35 +0100

    added camera functionality

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  currentImage: any;
+
+  constructor(private camera: Camera) {}
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((ImageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + ImageData;
+    }, (err) => {
+      console.log('Camera issue:', err);
+    });
+  }
+}
